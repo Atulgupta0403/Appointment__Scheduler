b341beebf8ca380e34c27f226d456131e1dd50f9
atulgupta0403 <atulgupta0403@gmail.com>
2024-10-29 18:01:32 +0530

    patient history

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -1,3 +1,4 @@
+const AppointementModel = require("../Models/AppointementModel")
 const userModel = require("../Models/userModel")
 const { ApiResponse } = require("../Utils/apiResponse")
 
@@ -5,8 +6,8 @@ const { ApiResponse } = require("../Utils/apiResponse")
 const admin = async (req, res) => {
 
     if (req.user) {
-        const user = await userModel.findOne({username : req.user.username , accountType : "admin"})
-        if(user){
+        const user = await userModel.findOne({ username: req.user.username, accountType: "admin" })
+        if (user) {
             const email = req.query.email
             if (!email) {
                 res.json(new ApiResponse(200, "Doctor email is required"))
@@ -16,14 +17,33 @@ const admin = async (req, res) => {
                 res.json(new ApiResponse(200, doctor, "Doctor deleted"))
             }
         }
-        else{
-            res.json(new ApiResponse(200 , "you are not authorized to delete any doctor details"))
+        else {
+            res.json(new ApiResponse(200, "you are not authorized to delete any doctor details"))
         }
     }
-    else{
-        res.json(new ApiResponse(200  , "please login"))
+    else {
+        res.json(new ApiResponse(200, "please login"))
     }
 }
 
 
-module.exports = { admin }
+const history = async (req, res) => {
+    if (req.user) {
+        const admin = await userModel.findOne({ username: req.user.username , accountType : "admin"})
+        if (!admin) {
+            res.json(new ApiResponse(200, admin, "Only admin are authorized to see the history"))
+        }
+        else {
+            const Patient_ID = req.query.Patient_ID;
+            const allAppoint = await AppointementModel.find({ Patient_ID: Patient_ID });
+            const length = allAppoint.length
+            res.json(new ApiResponse(200, allAppoint, `There are ${length} appointements total`))
+        }
+    }
+    else{
+        res.json(new ApiResponse(200, "please login"))
+    }
+}
+
+
+module.exports = { admin, history }
--- Routes/Appointement.js
+++ Routes/Appointement.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createAppointement, showAppointments, appointementReschedule } = require("../Controllers/AppointementController");
 const { isLoggedIn } = require("../Middlewares/isLoggedIn");
 const router = express.Router();
+const {history} = require("../Controllers/adminController")
 
 
 router.post("/createAppointment" , isLoggedIn , createAppointement)
@@ -9,5 +10,8 @@ router.post("/createAppointment" , isLoggedIn , createAppointement)
 router.get("/showAppointments" , isLoggedIn , showAppointments)
 
 router.patch("/appointment/reschedule" , isLoggedIn , appointementReschedule)
+
+router.get("/patient/history" , isLoggedIn , history)
+
 
 module.exports = router
