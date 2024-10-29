5f56c701c01e0460e711106b4f8765a1d82fe942
atulgupta0403 <atulgupta0403@gmail.com>
2024-10-29 19:22:21 +0530

    login with google done"

---
+++ Routes/loginGoogle.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const router = express.Router();
+const passport = require("passport");
+const jwt = require("jsonwebtoken")
+
+
+const GoogleStrategy = require("passport-google-oauth2").Strategy;
+
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/callback"
+    },
+        (accessToken, refreshToken, profile, done) => {
+            return done(null, profile)
+        }
+    )
+)
+
+passport.serializeUser((user, done) => done(null, user))
+passport.deserializeUser((user, done) => done(null, user))
+
+router.get("/" , 
+    passport.authenticate("google" , {scope : ["profile" , "email"]})
+)
+
+router.get("/callback" , 
+    passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const name = req.user.displayName;
+    const token = jwt.sign({name} , process.env.SECRET)
+    console.log(token)
+    res.redirect("/profile")
+  }
+)
+
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const passport = require('passport');
+const session = require("express-session")
 
 app.use(cookieParser())
 
@@ -9,9 +11,19 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+}))
+
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 app.get("/", (req, res) => {
-    res.send("slash page")
+    res.send("<a href='/auth/google'> Login with google </a>")
 })
 
 const register = require("./Routes/signUp");
@@ -19,14 +31,22 @@ const login = require("./Routes/login");
 const email = require("./Routes/email");
 const Appointement = require("./Routes/Appointement");
 const doctor = require("./Routes/doctor")
-const admin = require("./Routes/admin")
+const admin = require("./Routes/admin");
+const LoginGoogle = require("./Routes/loginGoogle")
 
 app.use("/signup", register)
 app.use("/login", login)
 app.use("/", email)
 app.use("/", Appointement)
-app.use("/" , doctor)
-app.use("/admin" , admin)
+app.use("/", doctor)
+app.use("/admin", admin)
+app.use("/auth/google" , LoginGoogle)
+
+
+
+app.get("/profile", (req, res) => {
+    res.send(`<h1>Welcome ${req.user.displayName} </h1>`)
+})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("App is listening at port 3000");
